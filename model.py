35fe53faa6f0e8c3409a185a7f8def2a4a22ef68
TrsNium <umenyan.mitou@gmail.com>
2017-10-20 00:59:44 +0900

    .

--- model.py
+++ model.py
@@ -7,28 +7,31 @@ class model():
         self.args = args
 
         with tf.variable_scope("MemN2N"):
-            self.q = tf.placeholder(dtype=tf.int32, shape=[None, args.vocab_size],  name="q_vector")
-            self.support_s = tf.placeholder(dtype=tf.int32, shape=[args.support_size, args.vocab_size], name="support_sentences")
-            self.labels = tf.placeholder(dtype=tf.float32, shape=[None, args.label_num], name="label")
+            self.q = tf.placeholder(dtype=tf.float32, shape=[None, args.vocab_size],  name="q_vector")
+            self.support_s = tf.placeholder(dtype=tf.float32, shape=[args.support_size, args.vocab_size], name="support_sentences")
+            self.labels = tf.placeholder(dtype=tf.float32, shape=[None, args.vocab_size], name="label")
 
-            q = identity(self.q, name="q")
-            for l in args.layer_num:
+            q = tf.identity(self.q, name="q")
+            for l in range(args.layer_num):
                 u_ = tf.layers.dense(q, self.args.embedding_size, name="embedding_B_{}".format(l))
-                range_u_ = tf.reshape(tf.tile(u_, args.support_size), (None, args.support_size, args.embedding_size))
-                
+                range_u_ = tf.reshape(tf.tile(u_, [1, args.support_size]), shape=[-1, args.support_size, args.embedding_size])
+
                 embedding_weight_A = tf.get_variable("embedding_weight_A_{}".format(l), shape=(args.vocab_size, args.embedding_size), dtype=tf.float32)
                 m_ = tf.matmul(self.support_s, embedding_weight_A)
 
                 embedding_weight_C = tf.get_variable("embedding_weight_C_{}".format(l), shape=(args.vocab_size, args.embedding_size), dtype=tf.float32)
                 c_ = tf.matmul(self.support_s, embedding_weight_C)
 
-                pi_ = tf.nn.softmax(tf.multiply(m_, range_u_))
-                q = tf.add(tf.multiply(c_, pi), range_u_)
+                pi_ = tf.nn.softmax(tf.multiply(m_, range_u_), dim=1)
 
-            logits = tf.layers.dense(q, args.label_num, name="logit")
-            self.loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=self.labels)
+                q = tf.reduce_sum(tf.add(tf.multiply(c_, pi_), range_u_), axis=1)
+
+            logits = tf.layers.dense(q, args.vocab_size, name="logit")
+            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=self.labels))
 
             self.output = tf.nn.softmax(logits)
+
+            self.ppp = tf.identity(pi_)
 
     def train(self):
         optimizer = tf.train.AdamOptimizer(self.args.lr).minimize(self.loss)
@@ -41,7 +44,7 @@ class model():
             summary_writer = tf.summary.FileWriter("saved", sess.graph)
             saver = tf.train.Saver(tf.global_variables())
 
-            for itr, (q_vec, label) for enumerate():
+            for itr in range(100):
 
                 if itr % 200 == 0:
                     saver.save(sess, "saved/model.ckpt")
