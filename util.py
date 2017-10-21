c2f84f64c7b08cc086c9c1d07c4808e41490660c
TrsNium <umenyan.mitou@gmail.com>
2017-10-21 16:37:07 +0900

    .

--- util.py
+++ util.py
@@ -23,10 +23,43 @@ def write_dict(data_path, dict_):
         fs.write("\n".join(dict_))
     return None
 
-def mk_train_function(batch_size, data_path, dict_path):
+def convert_sentence2bow(sentence, dict_):
+    bow = [0] * len(dict_)
+    for word in sentence.split(" "):
+        bow[dict_.index(word)] = 1
+    return bow
+
+def read_support_data(data_path):
+    with open(data_path) as fs:
+        sentences = fs.read().lower().replace(".", "")
+
+    support_data = sentences.split("\n")
+    return support_data
+
+def read_q_label_data(data_path):
+    with open(data_path) as fs:
+        lines = fs.read().lower().replace("?", "").split("\n")
+
+    q_ = []
+    labels = []
+    for line in lines:
+        splitted = line.split("\t")
+        q_.append(splitted[0])
+        labels.append(splitted[1])
+
+    return q_, labels
+
+
+def mk_train_function(batch_size, support_path, q_label_path, dict_path):
+    #q_label_path: question and answer of its data path
+    #support path: support sentences data path
+
     dict_ = read_dict(dict_path)
+    support_data = read_support_data(support_path)
+    q_, label = read_q_label_data(q_label_path)
 
     def train_function():
+        dump = []
         while True:
 
             yield
